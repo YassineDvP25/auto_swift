33fdf730f9d6faa0b5298af9be11473caf5e91a1
Yassine El Anssari <yassinedvp25@gmail.com>
2025-08-18 10:18:31 +0100

    style(home): refine theming and improve car UI components

--- lib/features/home/widgets/custom_top_bar.dart
+++ lib/features/home/widgets/custom_top_bar.dart
@@ -9,21 +9,27 @@ class CustomTopBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return   Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    CircleAvatar(
-                      radius: 20.r,
-                      backgroundImage: NetworkImage(
-                        'https://i.pinimg.com/1200x/7d/47/d1/7d47d1d2455f248981f41feb424d3867.jpg',
-                      ),
-                    ),
-                    CustomText(
-                      text: 'Boston , New York',
-                      color: AppColors.mainDarkGrey,
-                    ),
-                    Icon(CupertinoIcons.circle_grid_3x3, size: 25.sp),
-                  ],
-                );
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        CircleAvatar(
+          radius: 19.r,
+          backgroundColor: Colors.white,
+          child: CircleAvatar(
+            
+            radius: 18.r,
+            backgroundImage: NetworkImage(
+              'https://i.pinimg.com/1200x/7d/47/d1/7d47d1d2455f248981f41feb424d3867.jpg',
+            ),
+          ),
+        ),
+        CustomText(text: 'Boston , New York', color: AppColors.mainWhite),
+        Icon(
+          CupertinoIcons.circle_grid_3x3,
+          size: 25.sp,
+          color: AppColors.mainWhite,
+        ),
+      ],
+    );
   }
 }
